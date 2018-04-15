a20f5437a21d0fbea368b1e15ae7a15ba1b938b2
Vibhor <bijoy.vibhor19@gmail.com>
2018-04-15 09:06:50 +0530

    Update Details.java

--- Details.java
+++ Details.java
@@ -1,4 +1,4 @@
-package com.example.vibhor.daid;
+package com.example.vibhor.handicapped;
 
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
@@ -12,16 +12,15 @@ public class Details extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
     }
+
     public void OpenDetails(View view) {
         switch (view.getId()) {
-            case R.id.b_read:
-                startActivity(new Intent(this, Read.class));
+            case R.id.b_goep:
+                startActivity(new Intent(this, Academia.class));
                 break;
-            case R.id.b_listen:
-                startActivity(new Intent(this, Listen.class));
+            case R.id.b_jobedu:
+                startActivity(new Intent(this, JOBS.class));
                 break;
-
-
         }
     }
 }
